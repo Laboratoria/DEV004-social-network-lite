23df53fa51d81e84bc7937656e7f32a793a45e2f
Sergio Sinuco <ssinuco@gmail.com>
2023-04-05 15:47:18 -0500

    localstorage services

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,19 +6,3 @@ export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
 };
-
-init();
-register('ssinuco@gmail.com', '123456');
-login('ssinuco@gmail.com', '123456');
-const id1 = createPost('Hola mundo', 'ssinuco@gmail.com');
-const id2 = createPost('Hola mundo 2', 'ssinuco@gmail.com');
-setTimeout(() => {
-  deletePost(id1);
-}, 30000);
-setTimeout(() => {
-  editPost(id2, 'Hola mundo 2 editado');
-}, 60000);
-const posts = getPosts();
-console.log(posts);
-
-
